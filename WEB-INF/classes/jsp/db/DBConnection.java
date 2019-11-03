709ff99906fa3cb87b954323644fd7da84e45743
kabsung <kabsung3@naver.com>
2019-11-03 13:15:41 +0900

    Updata_mainLoginToModal_and ect

--- WEB-INF/classes/jsp/db/DBConnection.java
+++ WEB-INF/classes/jsp/db/DBConnection.java
@@ -3,22 +3,24 @@ package jsp.db;
 import java.sql.*;
 
 public class DBConnection {
+
 	public static Connection SqlConnectionStart() {
 		Connection conn = null;
 
 		try {
-       		Class.forName("com.mysql.jdbc.Driver");
-        	
-	        String url = "jdbc:mysql://106.10.52.145/dockingJob";
-	        String id = "20175120";
-	        String passwd = "20175120";
-	        
-    	    conn = DriverManager.getConnection(url, id, passwd);
-		
-		}catch(Exception e) {
+			Class.forName("com.mysql.jdbc.Driver");
+
+			String url = "jdbc:mysql://106.10.52.145/dockingJob";
+			String id = "20175120";
+			String passwd = "20175120";
+
+			conn = DriverManager.getConnection(url, id, passwd);
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return conn;
 	}
+
 }
